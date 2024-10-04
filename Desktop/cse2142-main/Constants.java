1f17f67a7a70e15e6b51fdece1f77df52d3bacba
Redoan_Zim <redoanzim2002@gmail.com>
2024-10-04 22:34:00 +0600

    step1

---
+++ Desktop/cse2142-main/Constants.java
@@ -0,0 +1,15 @@
+public class Constants {
+    public static final String Student_List = "students.txt";
+    public static final String ShowAll = "a";
+    public static final String ShowRandom = "r";
+    public static final String AddEntry = "+";
+    public static final String FindEntry = "?";
+    public static final String ShowCount = "c";
+    public static final  String Loading = "Loading data ...";
+    public static final String Loaded = "Data Loaded.";
+    public static final String Wrong_Argument = "Please write right argument(a | r | c | +WORD | ?WORD).";
+    public static final String SplitAt = ", ";
+    public static final String Date_Format = "dd/mm/yyyy-hh:mm:ss a";
+    public static final String Found_Massage = "We found it!";
+
+}
